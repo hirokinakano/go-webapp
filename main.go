42e0ed465afe23514ce9ea07e7e3f3ec23fa91cb
hirokinakano <nkn12151018@gmail.com>
2020-09-21 16:03:28 +0900

    Create Users Response

--- main.go
+++ main.go
@@ -1,60 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests()  {
-	//http.HandleFunc("/", homePage)
-	//http.HandleFunc("/articles", returnAllArticles)
-
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
+type User struct {
+	ID int
+	FirstName string
+	LastName string
 }
 
 func main() {
-	handleRequests()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/users", findAllUsers)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-type Article struct {
-	Id int `json:"id"`
-	Title string `json:Title`
-	Desc string `json:"desc"`
-	Content string `json:"content"`
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
 
-type Articles []Article
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{}
-	for i := 0; i < 10; i++ {
-		title := "Hello_%d"
-		articles = append(
-			articles,
-			Article{Title: fmt.Sprintf(title, i), Desc: "Article Description", Content: "Article Content"})
+func findAllUsers(w http.ResponseWriter, r *http.Request) {
+	var userList = []User{
+		User{ID: 1, FirstName: "Taro", LastName: "Yamada"},
+		User{ID: 2, FirstName: "Jiro", LastName: "Sato"},
 	}
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(articles)
-}
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request){
-	vars := mux.Vars(r)
-	key := vars["id"]
-	fmt.Fprintf(w, vars)
-	fmt.Fprintf(w, "Key: " + key + "\n")
-	//article := Article{Id: key, Title: "タイトル" }
-	//json.NewEncoder(w).Encode(article)
+	response, _ := json.Marshal(userList)
 
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
